2f83723f4c066cb784eed8833fab61c6a246d861
GNooNJo <jjjchoon@nate.com>
2024-02-04 04:26:49 +0900

    add: addMail component

---
+++ src/components/mail/AddMail.jsx
@@ -0,0 +1,14 @@
+import AddMailContainer from "components/style/AddMailStyle";
+import ArtistList from "./ArtistList";
+import AddForm from "./AddForm";
+
+function AddMail() {
+  return (
+    <AddMailContainer>
+      <ArtistList />
+      <AddForm />
+    </AddMailContainer>
+  );
+}
+
+export default AddMail;
