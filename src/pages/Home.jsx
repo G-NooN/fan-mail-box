f305efb3d305b6821e6457588d2ab13517dc65a8
GNooNJo <jjjchoon@nate.com>
2024-02-04 00:07:30 +0900

    add: Router component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "components/layout/Header";
+import MainPage from "components/layout/MainPage";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <MainPage />
+    </>
+  );
+};
+
+export default Home;
