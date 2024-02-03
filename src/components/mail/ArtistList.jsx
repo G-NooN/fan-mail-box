83f96f18945bc11a470205e178522598e890dc33
GNooNJo <jjjchoon@nate.com>
2024-02-04 04:27:07 +0900

    add: ArtistList component

---
+++ src/components/mail/ArtistList.jsx
@@ -0,0 +1,34 @@
+import {
+  ArtistsContainer,
+  ArtistListTitle,
+  MaleArtist,
+  FemaleArtist,
+} from "components/style/ArtistListStyle";
+import { useState } from "react";
+
+const ArtistList = () => {
+  const [activeArtist, setActiveArtist] = useState("방현재");
+
+  const onClickArtist = (event) => {
+    setActiveArtist(event.target.innerText);
+  };
+
+  return (
+    <>
+      <ArtistListTitle>Our Artists</ArtistListTitle>
+      <ArtistsContainer onClick={onClickArtist}>
+        <MaleArtist $activeArtist={activeArtist}>방현재</MaleArtist>
+        <FemaleArtist $activeArtist={activeArtist}>장미</FemaleArtist>
+        <FemaleArtist $activeArtist={activeArtist}>장윤서</FemaleArtist>
+        <MaleArtist $activeArtist={activeArtist}>최원장</MaleArtist>
+        <MaleArtist $activeArtist={activeArtist}>이재상</MaleArtist>
+        <MaleArtist $activeArtist={activeArtist}>윤창식</MaleArtist>
+        <MaleArtist $activeArtist={activeArtist}>권혁우</MaleArtist>
+        <MaleArtist $activeArtist={activeArtist}>김병연</MaleArtist>
+        <FemaleArtist $activeArtist={activeArtist}>박가현</FemaleArtist>
+      </ArtistsContainer>
+    </>
+  );
+};
+
+export default ArtistList;
---
+++ src/components/style/ArtistListStyle.jsx
@@ -0,0 +1,78 @@
+import styled, { css } from "styled-components";
+
+const ArtistsContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 600px;
+  margin: 20px 0;
+  padding: 10px;
+  background-color: whitesmoke;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
+const ArtistListTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 900;
+`;
+
+const MaleArtist = styled.li`
+  width: 160px;
+  margin: 10px;
+  padding: 10px;
+
+  ${(props) => {
+    if (props.$activeArtist === props.children) {
+      return css`
+        background-color: aliceblue;
+      `;
+    }
+    return css`
+      background-color: white;
+    `;
+  }}
+
+  border: 1px solid black;
+  border-radius: 5px;
+  text-align: center;
+  font-weight: 700;
+  color: dodgerblue;
+  cursor: pointer;
+
+  &:hover {
+    background-color: aliceblue;
+    transform: scale(1.05);
+  }
+`;
+
+const FemaleArtist = styled.li`
+  width: 160px;
+  margin: 10px;
+  padding: 10px;
+
+  ${(props) => {
+    if (props.$activeArtist === props.children) {
+      return css`
+        background-color: lavenderblush;
+      `;
+    }
+    return css`
+      background-color: white;
+    `;
+  }}
+
+  border: 1px solid black;
+  border-radius: 5px;
+  text-align: center;
+  font-weight: 700;
+  color: hotpink;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lavenderblush;
+    transform: scale(1.05);
+  }
+`;
+
+export { ArtistsContainer, ArtistListTitle, MaleArtist, FemaleArtist };
