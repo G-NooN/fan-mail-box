c38848ad0cacb12e8c6c408fc649f762567d86b0
GNooNJo <jjjchoon@nate.com>
2024-02-05 04:33:10 +0900

    feat: add Mail Detail component

---
+++ src/components/styles/MailDetailStyle.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+const MailDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 800px;
+  margin: 20px 0;
+  padding: 20px;
+  background-color: floralwhite;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
+const WriterInfoSection = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+const ReceiverInfoSection = styled.div`
+  display: flex;
+  margin: 0 10px;
+  padding: 5px;
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const FullMailContent = styled.p`
+  width: 720px;
+  margin: 0 20px;
+  padding: 10px;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 5px;
+  line-height: 1.5;
+`;
+
+const EditMailArea = styled.textarea`
+  width: 720px;
+  margin: 0 20px;
+  padding: 10px;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 5px;
+  resize: none;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 1.5;
+`;
+
+export {
+  MailDetailContainer,
+  WriterInfoSection,
+  ReceiverInfoSection,
+  FullMailContent,
+  EditMailArea,
+};
