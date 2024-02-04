5e642180c2feed43037f05d203cd75c76ffcf59d
GNooNJo <jjjchoon@nate.com>
2024-02-05 04:40:05 +0900

    feat: NoMail component

--- src/components/mails/MailListSection.jsx
+++ src/components/mails/MailListSection.jsx
@@ -1,17 +1,20 @@
 import { SectionContainer, SectionTitle } from "components/styles/GlobalStyle";
 import { MailListContainer } from "components/styles/MailStyle";
 import Mail from "./Mail";
+import NoMail from "./NoMail";
 
-const MailListSection = ({ activeArtist, mailList }) => {
-  const filteredMailList = mailList.filter((mail) => mail.writedTo === activeArtist);
-
+const MailListSection = ({ defaultAvatar, activeArtist, filteredMailList, options }) => {
   return (
     <SectionContainer>
       <SectionTitle>Mail LIST</SectionTitle>
       <MailListContainer>
-        {filteredMailList.map((mail) => (
-          <Mail mail={mail} />
-        ))}
+        {filteredMailList.length === 0 ? (
+          <NoMail activeArtist={activeArtist} />
+        ) : (
+          filteredMailList.map((mail) => (
+            <Mail key={mail.id} mail={mail} defaultAvatar={defaultAvatar} options={options} />
+          ))
+        )}
       </MailListContainer>
     </SectionContainer>
   );
---
+++ src/components/styles/NoMailStyle.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const NoMailContainer = styled.div`
+  text-align: center;
+  width: 600px;
+  margin: 10px 0;
+  padding: 10px;
+  background-color: whitesmoke;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
+export { NoMailContainer };
