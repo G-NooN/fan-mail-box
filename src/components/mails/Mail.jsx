0aee3d05cbec28008a5cd522312b29e7a7e0bf43
GNooNJo <jjjchoon@nate.com>
2024-02-05 04:37:32 +0900

    rename: change nickname <p> -> <Nickname> component

--- src/components/mails/Mail.jsx
+++ src/components/mails/Mail.jsx
@@ -1,34 +1,20 @@
-import {
-  MailContainer,
-  Avatar,
-  MailInfo,
-  WriterInfo,
-  MailContent,
-} from "components/styles/MailStyle";
+import { Avatar, Nickname, WriterInfo } from "components/styles/GlobalStyle";
+import { MailContainer, MailInfo, MailContent } from "components/styles/MailStyle";
+import { useNavigate } from "react-router-dom";
 
-import defaultAvatar from "assets/defaultAvatar.png";
-
-const Mail = ({ mail }) => {
-  const options = {
-    hour12: false,
-    weekday: "short",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  };
+const Mail = ({ mail, defaultAvatar, options }) => {
   const formattedDate = new Date(mail.createdAt).toLocaleDateString("ko-KR", options);
 
+  const navigate = useNavigate();
+
   return (
-    <MailContainer>
+    <MailContainer onClick={() => navigate(`/detail/${mail.id}`)}>
       <Avatar>
         <img src={mail.avatar || defaultAvatar} alt="avatar" />
       </Avatar>
       <MailInfo>
         <WriterInfo>
-          <p>{mail.nickname}</p>
+          <Nickname>{mail.nickname}</Nickname>
           <time>{formattedDate}</time>
         </WriterInfo>
         <MailContent>{mail.content}</MailContent>
